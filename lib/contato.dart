4cee7286185260ef4c07eba194e78fa1cd9e7953
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-23 22:22:07 -0300

    atv aula list

---
+++ lib/contato.dart
@@ -0,0 +1,16 @@
+class Contato{
+  String nome;
+  String email;
+  Contato(this.nome, this.email);
+}
+
+List<Contato> contatos = [
+  Contato('Guilherme', 'teste@gmail.com'),
+  Contato('Gustavo', 'teste2@gmail.com'),
+  Contato('Gustavo', 'teste2@gmail.com'),
+  Contato('Renato', 'teste3@gmail.com'),
+  Contato('Matheus', 'teste5@gmail.com'),
+  Contato('Paula', 'teste6@gmail.com'),
+  Contato('Luiza', 'teste7@gmail.com'),
+];
+
